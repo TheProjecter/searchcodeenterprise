be92f0af365952316192d303c5a746465268269e
flavio.henso <flavio.henso@ca8c216a-429f-724e-ff73-58d1fea2a2b8>
2010-04-27 00:28:54 +0000

    configuração do banco de dados Inserção do jar do mysql alteração do objeto modelo Inserção do controle de acesso

--- searchCodeEnterprise/grails-app/domain/br/com/searchCodeEnterprise/model/Codigo.groovy
+++ searchCodeEnterprise/grails-app/domain/br/com/searchCodeEnterprise/model/Codigo.groovy
@@ -1,14 +1,22 @@
 package br.com.searchCodeEnterprise.model
 
-class Codigo {
-	static searchable = true
-	
+import java.io.Serializable;
+
+class Codigo implements Serializable{
+	static searchable = {
+		alias "foobar"
+	    subIndex "fb"
+		constant name: "type", value: "some foobar"
+		constant name: "noise", values: ["squawk", "shriek"]
+		spellCheck "include"
+		}
+
 	File fonte
 	String descricao
 	String autor
 	
 	static constraints = {
-		descricao()
+		
 		}
 	
 	
